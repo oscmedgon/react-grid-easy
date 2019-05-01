7dda87080a60c07d06a4322c98434b2093023df3
Óscar Medina <oscmedgon@gmail.com>
2019-05-01 14:45:12 +0200

    added deploy script

---
+++ deployGH.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+git add *
+git commit -m "Commit before deploy gh pages"
+git push
+git branch -f gh-pages
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+git checkout gh-pages
+rm index.html main.js
+git merge $BRANCH gh-pages
+ls -la
+
+rm -rf ./lib/ *.md ./*.js ./sass
+mv -F ./demo/dist/* .
+rm -rf ./demo/
+
+git add .
+git commit -m "Deploy github pages"
+git push
+git checkout $BRANCH
+exit

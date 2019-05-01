154bc528138e683f09b664a18d03759cd5e34ada
devosc <oscmedgon@gmail.com>
2019-05-01 14:57:49 +0200

    Commit before deploy gh pages

--- deployGH.sh
+++ deployGH.sh
@@ -3,19 +3,19 @@
 git add *
 git commit -m "Commit before deploy gh pages"
 git push
-git branch -f gh-pages
+git branch gh-pages
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 git checkout gh-pages
 rm index.html main.js
 git merge $BRANCH
-#
-#rm -rf ./lib/ *.md ./*.js ./sass
-#mv -F ./demo/dist/* .
-#rm -rf ./demo/
-#
-#git add .
-#git commit -m "Deploy github pages"
-#git push
-#git checkout $BRANCH
-#exit
+
+rm -rf ./lib/ *.md ./*.js ./sass
+mv -F ./demo/dist/* .
+rm -rf ./demo/
+
+git add .
+git commit -m "Deploy github pages"
+git push
+git checkout $BRANCH
+exit
